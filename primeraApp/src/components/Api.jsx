a3734f4d3318aaac4cfaa10c79e4a71447633083
Roberto <robertocariolo94@gmail.com>
2024-08-17 16:48:39 -0400

    a

---
+++ primeraApp/src/components/Api.jsx
@@ -0,0 +1,42 @@
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+
+export default function App () {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '9ef4723b5943f72f6b3f216e27adb13a',
+    lat: '48.137154',
+    lon: '11.576124',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
+  const customStyles = {
+    fontFamily:  'Helvetica, sans-serif',
+    gradientStart:  '#0181C2',
+    gradientMid:  '#04A7F9',
+    gradientEnd:  '#4BC4F7',
+    locationFontColor:  '#FFF',
+    todayTempFontColor:  '#FFF',
+    todayDateFontColor:  '#B5DEF4',
+    todayRangeFontColor:  '#B5DEF4',
+    todayDescFontColor:  '#B5DEF4',
+    todayInfoFontColor:  '#B5DEF4',
+    todayIconColor:  '#FFF',
+    forecastBackgroundColor:  '#FFF',
+    forecastSeparatorColor:  '#DDD',
+    forecastDateColor:  '#777',
+    forecastDescColor:  '#777',
+    forecastRangeColor:  '#777',
+    forecastIconColor:  '#4BC4F7',
+  };
+  return (
+    <ReactWeather
+      theme={customStyles}
+      isLoading={isLoading}
+      errorMessage={errorMessage}
+      data={data}
+      lang="en"
+      locationLabel="Munich"
+      unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+      showForecast
+    />
+  );
+};
